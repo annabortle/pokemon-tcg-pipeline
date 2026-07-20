e013daa4f03f69a859720d7f1e6c68a696e8f6ea
annabortle <anbortle@gmail.com>
2026-07-20 14:48:06 -0400

    add functions to search cards, sets by name

--- scripts/data_ingestion_requests.py
+++ scripts/data_ingestion_requests.py
@@ -10,7 +10,21 @@ headers = {
     'X-Api-Key': api_key,
 }
 
-response = requests.get('https://api.pokemontcg.io/v2/cards/xy1-1', headers=headers)
+BASEURL = 'https://api.pokemontcg.io/v2'
 
-print(response.status_code)
-print(response.text)
+def sendRequest(requestURL):
+    response = requests.get(requestURL, headers=headers)
+    return response.text
+
+def getPokemonByName(name):
+    cardQuery = '/cards?q=name:'
+    pokemon = f'"{name}"'
+    requestURL = BASEURL + cardQuery + pokemon
+    return sendRequest(requestURL)
+
+def getSetByName(name):
+    cardQuery = '/sets?q=name:'
+    set = f'"{name}"'
+    requestURL = BASEURL + cardQuery + set
+    print(requestURL)
+    return sendRequest(requestURL)
